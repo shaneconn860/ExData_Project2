ad67806988449b8a2e966bfa3e014f9a2c2b3164
Shane Connaughton <shaneconn86@gmail.com>
2020-04-06 11:08:53 +0100

    Added title to Plot

--- plot4.R
+++ plot4.R
@@ -32,7 +32,7 @@ coal <- SCC[grepl("coal", SCC$Short.Name, ignore.case = TRUE),]
 coal2 <- NEI[NEI$SCC %in% coal$SCC,]
 totalCoal <- aggregate(Emissions ~ year + type, coal2, sum)
 
-ggplot(totalCoal, aes(year, Emissions, type)) + geom_point() + geom_line()
+ggplot(totalCoal, aes(year, Emissions, type)) + geom_point() + geom_line() + ggtitle("Total Coal Emissions in US")
 
 # Save to png file
 dev.copy(png, file="plot4.png", height=480, width=480)
